459688fefdf1b8e274d53167a18314396487325f
tatsu07 <tatsu.tora.1986@gmail.com>
2015-02-19 23:12:34 +0900

    set JAVA_HOME

--- chef-repo/site-cookbooks/java/recipes/default.rb
+++ chef-repo/site-cookbooks/java/recipes/default.rb
@@ -6,11 +6,32 @@
 #
 # All rights reserved - Do Not Redistribute
 %W(
-  java-1.8.0-openjdk
-  java-1.8.0-openjdk-devel
-  java-1.8.0-openjdk-src
+  java-#{node['java']['version']}-openjdk
+  java-#{node['java']['version']}-openjdk-devel
+  java-#{node['java']['version']}-openjdk-src
 ).each do |package_name|
   package package_name do
     action :install
   end
 end
+
+bash "java_home" do
+  #user 'vagrant'
+  #environment "JAVA_HOME" => '/usr/lib/jvm/jre'
+  code <<-SHELL
+    echo "export JAVA_HOME=/usr/lib/jvm/jre" >> /home/vagrant/.bash_profile
+  SHELL
+  not_if "grep JAVA_HOME /home/vagrant/.bash_profile"
+end
+
+#template ".bash_profile" do
+#  user 'vagrant'
+#  owner 'vagrant'
+#  group 'vagrant'
+#  not_if "grep JAVA_HOME /home/vagrant/.bash_profile"
+#end
+#file "~/.bash_profile" do
+#  content <<-EOS
+#    export JAVA_HOME=/usr/lib/jvm/jre
+#  EOS
+#end
